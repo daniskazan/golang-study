881c70d0dda8ada8b7a261f7a00a6edf86b978db
danis <sharafiev@zvonok.com>
2023-11-30 19:53:04 +0300

    study

---
+++ basics/20/maps.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"maps"
+)
+
+func main() {
+
+	// To create an empty map, use the builtin make: make(map[key-type]val-type).
+	m := make(map[string]int)
+
+	// Set key/value pairs using typical name[key] = val syntax.
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+
+	v1 := m["k1"]
+	fmt.Println("v1:", v1)
+
+	// If the key doesn’t exist, the zero value of the value type is returned.
+	v3 := m["k3"]
+	fmt.Println("v3:", v3)
+
+	// The builtin len returns the number of key/value pairs when called on a map.
+	fmt.Println("len:", len(m))
+
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	// To remove all key/value pairs from a map, use the clear builtin.
+	clear(m)
+	fmt.Println("map:", m)
+
+	_, isK2KeyPresent := m["k2"]
+	fmt.Println("isPresent:", isK2KeyPresent)
+
+	// You can also declare and initialize a new map in the same line with this syntax.
+	n := map[string]int{"foo": 1, "bar": 2}
+	fmt.Println("map:", n)
+
+	// The maps package contains a number of useful utility functions for maps.
+	n2 := map[string]int{"foo": 1, "bar": 2}
+	if maps.Equal(n, n2) {
+		fmt.Println("n == n2")
+	}
+}
---
+++ basics/20/range.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+/*
+range iterates over elements in a variety of data structures.
+Let’s see how to use range with some of the data structures we’ve already learned.
+*/
+func main() {
+
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
+	}
+	fmt.Println("sum:", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "go" {
+		fmt.Println(i, c)
+	}
+}
