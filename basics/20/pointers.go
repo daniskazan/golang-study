ed340c4b801182f685a9f068074a85ee60797e82
daniskzn <danis.sharafiev@bk.ru>
2023-12-02 23:29:13 +0300

    upd

---
+++ basics/20/pointers.go
@@ -0,0 +1,33 @@
+/*
+Go supports pointers,
+allowing you to pass references to values and records within your program.
+
+We’ll show how pointers work in contrast to values with 2 functions: zeroval and zeroptr.
+zeroval has an int parameter, so arguments will be passed to it by value.
+zeroval will get a copy of ival distinct from the one in the calling function.
+*/
+
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println("initial:", i)
+
+	zeroval(i)
+	fmt.Println("zeroval:", i)
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i)
+
+	fmt.Println("pointer:", &i)
+}
